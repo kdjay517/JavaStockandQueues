cd91bbc142ea11dbf504f819efcdbc8928b41320
kdjay517 <kdjay517@gmail.com>
2021-11-18 04:43:52 +0530

    added pop and peek operations on stock

--- src/com/bridgelabz/stockandqueues/Runner.java
+++ src/com/bridgelabz/stockandqueues/Runner.java
@@ -7,13 +7,20 @@ public class Runner {
 	public static void main(String[] args) {
 		 Operations operate = new Operations();
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter 1 to push the data into stack");
+		System.out.println("Enter 1 to push the data into stack\n"+"Enter 2 pop operation \n"+
+										"Enter 3 for peek of stock");
 		switch (scanner.nextInt()) {
 		case 1:
 			operate.push();
 			break;
+		case 2:
+			operate.pop();
+			break;
+		case 3:
+			operate.peek();
+			break;
 		}
-		
+		scanner.close();
 	}
 
 }
--- src/com/bridgelabz/stockandqueues/StockAndQueues.java
+++ src/com/bridgelabz/stockandqueues/StockAndQueues.java
@@ -7,17 +7,59 @@ public class StockAndQueues {
 	public StockAndQueues(int size) {
 		super();
 		this.stock = new int [size];
-		this.top = 0;
+		this.top = -1;
 	}
 	
-	public void push (int data) {
-		stock[top] = data;
-		top++;
+	public boolean push(int data) {
+		if (isFull()) {
+			System.out.println("stock is overflow");
+			return false;
+		}
+		stock[++top] = data;
+		System.out.println("data is pushed into stack");
+		return true;
+	}
+	
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return top >= stock.length-1;
+	}
+
+	public int pop () {
+		if (isEmpty()) {
+			System.out.println("stock is underflow");
+			return 0;
+		}else {
+			int data = stock[top--];
+			stock[++top] = 0;
+			return data;
+		}
+	}
+	
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return top < 0;
+	}
+	
+	public int size() {
+		if(isEmpty()){
+			return 0;
+		}
+		return ++top;
+	}
+
+	public int peek() {
+		if (isEmpty()) {
+			System.out.println("stock is underflow");
+			return 0;
+		}
+		int data = stock[top];
+		return data;
 	}
 	
 	public void show() {
 		for(int n: stock) {
-			System.out.println(n+" ");
+			System.out.print(n+" ");
 		}
 	}
 
